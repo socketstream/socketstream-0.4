4bb95b08da19bad1d066bbebaaa0b24e8cb762e9
Owen Barnes <owen@socketstream.org>
2013-03-13 15:50:18 +0000

    Fixed script so it works on Ubuntu. Thanks @missinglink

--- example_app/npm_link_all.sh
+++ example_app/npm_link_all.sh
@@ -4,11 +4,8 @@ for file in ../mods/*
 
 do
   if [ -d "$file" ]; then
-    IN="$file"
-    set -- "$IN" 
-    IFS="/"; declare -a Array=($*) 
-    echo "${Array[2]}" 
-    sudo npm link "${Array[2]}";
+    MODULE=`basename $file`
+    sudo npm link $MODULE
     continue
   fi
 done
